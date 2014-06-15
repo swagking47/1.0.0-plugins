57243d3df9d7b2f6502b31b8259aa1ca4370d750
swagking47 <boyswag47@gmail.com>
2014-06-15 12:45:08 -0400

    Update Main.php

--- BanHammer/src/swagking47/BanHammer/Main.php
+++ BanHammer/src/swagking47/BanHammer/Main.php
@@ -1,1 +1,13 @@
 <php
+
+namespace swagking47\BanHammer
+
+use pocketmine\permission\BanList;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerInteractEvent 
+use pocketmine\event\player\PlayerItemHeldEvent 
+
