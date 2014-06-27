e619b024a7cea3c2586e38feea2d4ef7b4edb34b
swagking47 <boyswag47@gmail.com>
2014-06-27 15:26:04 -0400

    Delete Main.php

--- ArmorPackage.php/src/swagboy47/ArmorPackage/Main.php
+++
@@ -1,8 +0,0 @@
-<php
- 
- namespace swagboy47/;
-
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerItemHeldEvent;
-use pocketmine\event\entity\EntityMoveEvent;
