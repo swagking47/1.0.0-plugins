4d20839e1b04919ed7dc986687fce912fcc31f7b
swagking47 <boyswag47@gmail.com>
2014-06-28 12:21:58 -0400

    Update Main.php

--- MobSpawner/src/MobSpawner/Main.php
+++ MobSpawner/src/MobSpawner/Main.php
@@ -1,10 +1,30 @@
 <?php
 
 namespace MobSpawner;
-use pocketmine\event\block\BlockPlaceEvent;
-use pocketmine\event\Listener;
-use pocketmine\level;
+use pocketmine\entity\Entity;
+use pocketmine\entity\ProjectileSource;
 use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\network\protocol
 use pocketmine\plugin\PluginBase;
-use pocketmine\network\protocol\AddMobPacket;
-use 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+
+
+class Main extends PluginBase implements Listener{
+   private $ticks;
+    private $Mob;
+  public function onEnable(){
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new Mob($this), *20);
+		 $config = $this->getConfig();
+        $this->ticks = $config->get("ticks");
+        $this->Mob = $config->get("Mob");
+        $this->getLogger()->info("MobSpawner has been enabled.");
+  }
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+		switch($cmd->getName()){
+			case "ms":
+			  
+
+  
