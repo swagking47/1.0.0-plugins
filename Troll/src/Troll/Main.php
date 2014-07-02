09cff02a38b3ba7f1f9333ffa109b0341aaded82
swagking47 <boyswag47@gmail.com>
2014-07-02 18:47:41 -0400

    Update Main.php

--- Troll/src/Troll/Main.php
+++ Troll/src/Troll/Main.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace Troll;
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityInventoryChangeEvent;
+use pocketmine\event\entity\EntityMoveEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\entity\EntityArmorChangeEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\entity\Entity;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+public function onEnable()
+
+           
