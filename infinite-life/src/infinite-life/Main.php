1ee7de1326560069c82252ddc1f86a6413fde1a0
swagking47 <boyswag47@gmail.com>
2014-07-04 00:00:13 -0400

    Update Main.php

--- infinite-life/src/infinite-life/Main.php
+++ infinite-life/src/infinite-life/Main.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace infinite-life;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class Main extends PluginBase{
+    public function onEnable(){
+      
