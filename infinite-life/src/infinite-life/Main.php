18840cdf861aba8c716d85795880e6efd329b1b8
swagking47 <boyswag47@gmail.com>
2014-07-08 16:27:18 -0400

    Update Main.php

--- infinite-life/src/infinite-life/Main.php
+++ infinite-life/src/infinite-life/Main.php
@@ -6,33 +6,44 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
 
 class Main extends PluginBase{
     public function onEnable(){
          $this->getLogger()->info("infinite-life has been enabled.");
+         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
      public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
 		switch($cmd->getName()){
-			case "InfiniteLife":
-				    if($args[0] == "on" and $sender->hasPermission("InfiniteLife.Give")){
-				        $player = $this->getPlayer($args[0]);
-				        $player->setMaxHealth(1000000000000000 * 100000000000000000 * 10000000000000 * 10000000000 * 100000000000000000000000000000000 * 1000000000000000000000000000000000000 * 1000000000000000000000000000000000000000000000000 * 1000000000000000000000000000000000000000000000000000000000000 * 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 * 1000000000000000000000000000);
-				        $player->setHealth($player->getMaxHealth());
-				    }
-				    if($args[0] == "off" and $sender->hasPermission("InfiniteLife.Give")){
-				        $player->setMaxHealth(20);
-				        $player->setHealth($player->getMaxHealth());
-				    }
+			case "godemode":
+if($args[0] == "on" and $sender->hasPermission("godmode.give"){
+$player = $this->getPlayer($args[1]);
+	$this->getConfig->set($player->getName(), true);
+	$this->getConfig->save();
+	$sender->sendMasssge("godemode have been enabled for $args[1]");
+}
+if($args[0] == "off" and $sender->hasPermission("godmode.give"){
+$player = $this->getPlayer($args[1]);
+	$this->getConfig->set($player->getName(), false);
+	$this->getConfig->save();
+	$sender->sendMasssge("godemode have been disabled for $args[1]");
+}
+}
+}
+/** 
+     * @param EntityDamageEvent $data
+     * 
+     * @priority NORMAL
+     * @ignoreCalcelled false
+     */
+public function onEntityDamageEvent(EntityDamageEvent $event, Entity $entity){
+	if(($player = $event->getEntity()) instanceof Player){
+		if($this->getConfig->exists($player->getName())){
+		if($this->getConfig->get($player->getName()) = true){
+			$event->setCancelled(true);
+		}	
 		}
-     }
-		 public function onEnable(){
-         $this->getLogger()->info("infinite-life has been disabled.");
-    }
-     }
-
-				         
-			
-				             
-				             
-				             
-      
+	}
+}
+}
